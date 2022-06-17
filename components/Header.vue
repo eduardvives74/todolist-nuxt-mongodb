fa6d1bd4e1b9a603455cab594485c608f678ceb9
Eduard Vives <evives@redventures.com>
2022-06-17 14:46:31 -0500

    Added SASS and ENV Support, Added Layout and Header Component

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header id="header" class="py-1rem">
+    <div class="container d-grid">
+      <div class="col-4">
+        <img src="/logo-horizontal.png" alt="Logo Web" class="logo-web">
+      </div>
+      <div class="col-4"></div>
+      <div class="col-4"></div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+#header {
+  background-color: white;
+  box-shadow: 10px 0 20px rgba($color: #000000, $alpha: 0.20);
+  position: sticky;
+  top: 0;
+  left: 0;
+  .logo-web {
+    height: 50px;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div id="page">
+    <Header />
+    <Nuxt />
   </div>
 </template>
 
